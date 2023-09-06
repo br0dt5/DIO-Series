02c6b98a4e78253161752993df374d3b47185331
Luan Zaniqueli <luanzaniqueli.carlos@yahoo.com.br>
2023-09-06 13:08:19 -0300

    Commit inicial

---
+++ Classes/SerieRepositorio.cs
@@ -0,0 +1,36 @@
+namespace DIO.Series;
+
+public class SeriesRepositorio : IRepositorio<Serie>
+{
+	private List<Serie> listaSerie = new List<Serie>();
+
+	public void Atualiza(int id, Serie entidade)
+	{
+		listaSerie[id] = entidade;
+	}
+
+	public void Exclui(int id)
+	{
+		listaSerie[id].Excluir();
+	}
+
+	public void Insere(Serie entidade)
+	{
+		listaSerie.Add(entidade);
+	}
+
+	public List<Serie> Lista()
+	{
+		return listaSerie;
+	}
+
+	public int ProximoId()
+	{
+		return listaSerie.Count;
+	}
+
+	public Serie RetornaPorId(int id)
+	{
+		return listaSerie[id];
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using static System.Console;
+
+namespace DIO.Series;
+
+public class Program
+{
+	static SeriesRepositorio repositorio = new SeriesRepositorio();
+
+	public static void Main(string[] args)
+	{
+		string opcaoUsuario = ObterOpcaoUsuario();
+
+		while (opcaoUsuario.ToUpper() != "X")
+		{
+			switch (opcaoUsuario)
+			{
+				case "1":
+					//ListarSeries();
+					break;
+				case "2":
+					//InserirSerie();
+					break;
+				case "3":
+					//AtualizarSerie();
+					break;
+				case "4":
+					//ExcluirSerie();
+					break;
+				case "5":
+					//VisualizarSerie();
+					break;
+				case "C":
+					Clear();
+					break;
+				default:
+					throw new ArgumentOutOfRangeException();
+			}
+		}
+	}
+
+	private static string ObterOpcaoUsuario()
+	{
+		WriteLine();
+		WriteLine("DIO Séries a seu dispor!!!");
+		WriteLine("Informe a opção desejada:");
+
+		WriteLine("1 - Listar séries");
+		WriteLine("2 - Inserir nova série");
+		WriteLine("3 - Atualiza série");
+		WriteLine("4 - Excluir série");
+		WriteLine("5 - Visualizar série");
+		WriteLine("C - Limpar Tela");
+		WriteLine("X - Sair");
+		WriteLine();
+
+		string opcaoUsuario = ReadLine().ToUpper();
+		WriteLine();
+		return opcaoUsuario;
+	}
+}
